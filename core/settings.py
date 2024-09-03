994cb346e01c20cb00704399f9a6b82d180b8f77
arthuurgab <adarocha060@gmail.com>
2024-09-03 10:02:43 -0300

    frontend

--- core/settings.py
+++ core/settings.py
@@ -108,7 +108,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Caminho para a pasta onde os arquivos estáticos coletados serão armazenados (normalmente usado em produção)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Diretórios adicionais para o Django procurar por arquivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
